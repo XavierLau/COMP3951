a25300f33811ae5bb4b801d7cfc2ef7514f2e1cb
Connor Phalen <Phalen.Connor@hotmail.co.uk>
2016-02-12 19:57:50 -0800

    Yeah

---
+++ Drawing_Application/Drawing_Application/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace Drawing_Application
+{
+    partial class FormMainCanvas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.drawingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rectangleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.circleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.squareToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.scribbleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.backgroundColourToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorPenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colourBrushToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colourGradientBrushToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.insideColourToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.outsideColourToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.drawingToolStripMenuItem,
+            this.toolsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(388, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.X)));
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(133, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // drawingToolStripMenuItem
+            // 
+            this.drawingToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.rectangleToolStripMenuItem,
+            this.circleToolStripMenuItem,
+            this.squareToolStripMenuItem,
+            this.scribbleToolStripMenuItem});
+            this.drawingToolStripMenuItem.Name = "drawingToolStripMenuItem";
+            this.drawingToolStripMenuItem.Size = new System.Drawing.Size(63, 20);
+            this.drawingToolStripMenuItem.Text = "Drawing";
+            // 
+            // rectangleToolStripMenuItem
+            // 
+            this.rectangleToolStripMenuItem.Name = "rectangleToolStripMenuItem";
+            this.rectangleToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.R)));
+            this.rectangleToolStripMenuItem.Size = new System.Drawing.Size(163, 22);
+            this.rectangleToolStripMenuItem.Text = "Rectangle";
+            this.rectangleToolStripMenuItem.Click += new System.EventHandler(this.selectBrushType);
+            // 
+            // circleToolStripMenuItem
+            // 
+            this.circleToolStripMenuItem.Name = "circleToolStripMenuItem";
+            this.circleToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.C)));
+            this.circleToolStripMenuItem.Size = new System.Drawing.Size(163, 22);
+            this.circleToolStripMenuItem.Text = "Circle";
+            this.circleToolStripMenuItem.Click += new System.EventHandler(this.selectBrushType);
+            // 
+            // squareToolStripMenuItem
+            // 
+            this.squareToolStripMenuItem.Name = "squareToolStripMenuItem";
+            this.squareToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.S)));
+            this.squareToolStripMenuItem.Size = new System.Drawing.Size(163, 22);
+            this.squareToolStripMenuItem.Text = "Square";
+            this.squareToolStripMenuItem.Click += new System.EventHandler(this.selectBrushType);
+            // 
+            // scribbleToolStripMenuItem
+            // 
+            this.scribbleToolStripMenuItem.Name = "scribbleToolStripMenuItem";
+            this.scribbleToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.Q)));
+            this.scribbleToolStripMenuItem.Size = new System.Drawing.Size(163, 22);
+            this.scribbleToolStripMenuItem.Text = "Scribble";
+            this.scribbleToolStripMenuItem.Click += new System.EventHandler(this.selectBrushType);
+            // 
+            // toolsToolStripMenuItem
+            // 
+            this.toolsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.backgroundColourToolStripMenuItem,
+            this.colorPenToolStripMenuItem,
+            this.colourBrushToolStripMenuItem,
+            this.colourGradientBrushToolStripMenuItem});
+            this.toolsToolStripMenuItem.Name = "toolsToolStripMenuItem";
+            this.toolsToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.toolsToolStripMenuItem.Text = "Tools";
+            // 
+            // backgroundColourToolStripMenuItem
+            // 
+            this.backgroundColourToolStripMenuItem.Name = "backgroundColourToolStripMenuItem";
+            this.backgroundColourToolStripMenuItem.Size = new System.Drawing.Size(191, 22);
+            this.backgroundColourToolStripMenuItem.Text = "Background Colour";
+            this.backgroundColourToolStripMenuItem.Click += new System.EventHandler(this.openColourDialog);
+            // 
+            // colorPenToolStripMenuItem
+            // 
+            this.colorPenToolStripMenuItem.Name = "colorPenToolStripMenuItem";
+            this.colorPenToolStripMenuItem.Size = new System.Drawing.Size(191, 22);
+            this.colorPenToolStripMenuItem.Text = "Colour Pen";
+            this.colorPenToolStripMenuItem.Click += new System.EventHandler(this.openColourDialog);
+            // 
+            // colourBrushToolStripMenuItem
+            // 
+            this.colourBrushToolStripMenuItem.Name = "colourBrushToolStripMenuItem";
+            this.colourBrushToolStripMenuItem.Size = new System.Drawing.Size(191, 22);
+            this.colourBrushToolStripMenuItem.Text = "Colour Brush";
+            this.colourBrushToolStripMenuItem.Click += new System.EventHandler(this.openColourDialog);
+            // 
+            // colourGradientBrushToolStripMenuItem
+            // 
+            this.colourGradientBrushToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.insideColourToolStripMenuItem,
+            this.outsideColourToolStripMenuItem});
+            this.colourGradientBrushToolStripMenuItem.Name = "colourGradientBrushToolStripMenuItem";
+            this.colourGradientBrushToolStripMenuItem.Size = new System.Drawing.Size(191, 22);
+            this.colourGradientBrushToolStripMenuItem.Text = "Colour Gradient Brush";
+            // 
+            // insideColourToolStripMenuItem
+            // 
+            this.insideColourToolStripMenuItem.Name = "insideColourToolStripMenuItem";
+            this.insideColourToolStripMenuItem.Size = new System.Drawing.Size(154, 22);
+            this.insideColourToolStripMenuItem.Text = "Inside Colour";
+            this.insideColourToolStripMenuItem.Click += new System.EventHandler(this.openColourDialog);
+            // 
+            // outsideColourToolStripMenuItem
+            // 
+            this.outsideColourToolStripMenuItem.Name = "outsideColourToolStripMenuItem";
+            this.outsideColourToolStripMenuItem.Size = new System.Drawing.Size(154, 22);
+            this.outsideColourToolStripMenuItem.Text = "Outside Colour";
+            this.outsideColourToolStripMenuItem.Click += new System.EventHandler(this.openColourDialog);
+            // 
+            // FormMainCanvas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.ClientSize = new System.Drawing.Size(388, 344);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FormMainCanvas";
+            this.Text = "Connor and Xavier\'s Magical Colour Application";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.FormMainCanvas_Paint);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.FormMainCanvas_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.FormMainCanvas_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.FormMainCanvas_MouseUp);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem drawingToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rectangleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem circleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem squareToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem scribbleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem backgroundColourToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem colorPenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem colourBrushToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem colourGradientBrushToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem insideColourToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem outsideColourToolStripMenuItem;
+    }
+}
+
---
+++ Drawing_Application/Drawing_Application/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Drawing_Application
+{
+    public partial class FormMainCanvas : Form
+    {
+        private bool   drawEnabled      = false;
+        private string selectedBrush    = "square";
+
+        private const int SHAPE_BOUNDS_IN = 50;
+        private const int SHAPE_BOUNDS_OUT = 75;
+
+        Point cursorStart;
+        Point cursorEnd;
+
+        private Color  penColour        = Color.DeepPink;
+        private Color  brushColour      = Color.Crimson;
+        private Color  backgroundColour = Color.White;
+        
+        public FormMainCanvas()
+        {
+            InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        /// <summary>
+        /// Detects which ToolStripMenuItem was clicked, brings up the 
+        /// and sets the ColorDialog to choose the colour, and sets
+        /// the appropriate Color depending on what was clicked.
+        /// </summary>
+        /// <param name="sender">
+        /// The object that started the event.
+        /// </param>
+        /// <param name="e">
+        /// The event arguments.
+        /// </param>
+        private void openColourDialog(object sender, EventArgs e)
+        {
+            try
+            {
+                ToolStripMenuItem clickedTool = (ToolStripMenuItem)sender;
+
+                ColorDialog colourPicker      = new ColorDialog();
+
+                switch (clickedTool.Name)
+                {
+                    case "backgroundColourToolStripMenuItem":
+                        if (colourPicker.ShowDialog() == DialogResult.OK)
+                        {
+                            backgroundColour    = colourPicker.Color;
+                            this.BackColor      = backgroundColour;
+                        }
+                        break;
+                    case "colorPenToolStripMenuItem":
+                        if (colourPicker.ShowDialog() == DialogResult.OK)
+                        {
+                            penColour           = colourPicker.Color;
+                        }
+                        break;
+                    case "colourBrushToolStripMenuItem":
+                        if (colourPicker.ShowDialog() == DialogResult.OK)
+                        {
+                            brushColour         = colourPicker.Color;
+                        }
+                        break;
+                    case "insideColourToolStripMenuItem":
+                        if (colourPicker.ShowDialog() == DialogResult.OK)
+                        {
+                            brushColour          = colourPicker.Color;
+                        }
+                        break;
+                    case "outsideColourToolStripMenuItem":
+                        if (colourPicker.ShowDialog() == DialogResult.OK)
+                        {
+                            penColour            = colourPicker.Color;
+                        }
+                        break;
+                    default:
+                        colourPicker.Color       = Color.DarkRed;
+                        break;
+                }
+            }
+            catch (InvalidCastException ex)
+            {
+                MessageBox.Show("Error on casting the colour picker to be the correct type.");
+            }
+            finally
+            {
+                MessageBox.Show("Back Colour:\t"    + backgroundColour.ToString() +
+                                "\nPen Colour:\t"   + penColour.ToString() +
+                                "\nBrush Colour:\t" + brushColour.ToString());
+            }
+        }
+
+        /// <summary>
+        /// Gets the brush type that was selected and assigns it to
+        /// the selectedBrush variable.
+        /// </summary>
+        /// <param name="sender">
+        /// The object that started the event.
+        /// </param>
+        /// <param name="e">
+        /// The event arguments.
+        /// </param>
+        private void selectBrushType(object sender, EventArgs e)
+        {
+            try
+            {
+                ToolStripMenuItem clickedBrush = (ToolStripMenuItem)sender;
+
+                switch (clickedBrush.Name)
+                {
+                    case "rectangleToolStripMenuItem":
+                        selectedBrush = "rectangle";
+                        break;
+                    case "circleToolStripMenuItem":
+                        selectedBrush = "circle";
+                        break;
+                    case "squareToolStripMenuItem":
+                        selectedBrush = "square";
+                        break;
+                    case "scribbleToolStripMenuItem":
+                        selectedBrush = "scribble";
+                        break;
+                    default:
+                        selectedBrush = "rectangle";
+                        break;
+                }
+            }
+            catch (InvalidCastException ex)
+            {
+                MessageBox.Show("Error on casting the brush to be the correct style.");
+            }
+            finally
+            {
+                MessageBox.Show("Selected Brush: " + selectedBrush);
+            }
+
+        }
+
+        private void FormMainCanvas_MouseDown(object sender, MouseEventArgs e)
+        {
+            drawEnabled = true;
+            cursorStart = new Point(Cursor.Position.X, Cursor.Position.Y);
+            this.Refresh();
+        }
+
+        private void FormMainCanvas_MouseUp(object sender, MouseEventArgs e)
+        {
+            drawEnabled = false;
+            cursorEnd = new Point(Cursor.Position.X, Cursor.Position.Y);
+        }
+
+        private void FormMainCanvas_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            if (drawEnabled)
+            {
+                switch (selectedBrush)
+                {
+                    case "rectangle":
+
+                        //draw lines connecting points (4 lines total)
+
+                        //fill in the lines with a rectangle
+
+                        break;
+                    case "circle":
+                        // draw brush method with circle parameter
+                        break;
+                    case "square":
+                        GraphicsPath pathGradient = new GraphicsPath();
+
+                        pathGradient.AddRectangle(new Rectangle(Cursor.Position.X, Cursor.Position.Y,
+                                                                SHAPE_BOUNDS_IN, SHAPE_BOUNDS_IN));
+
+                        PathGradientBrush pathGradientBrush = new PathGradientBrush(pathGradient);
+
+                        pathGradientBrush.CenterColor       = brushColour;
+                        pathGradientBrush.SurroundColors    = new Color[] { penColour };
+
+                        e.Graphics.FillRectangle(pathGradientBrush, Cursor.Position.X, Cursor.Position.Y, 
+                                                                    SHAPE_BOUNDS_OUT, SHAPE_BOUNDS_OUT);
+
+                        // draw brush method with square parameter
+                        break;
+                    case "scribble":
+                        // draw brush method with scribble parameter
+                        break;
+                    default:
+                        // Do not draw anything.
+                        break;
+                }
+            }
+
+        }
+
+        private void FormMainCanvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            this.Invalidate();
+        }
+    }
+}
